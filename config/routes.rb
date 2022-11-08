eb19d7bb3d0acb53273923461960f6a433a3a41b
StevenMetz <stevenm.codes@gmail.com>
2022-11-08 15:14:16 -0600

    all routes and methods are functional

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "/users" => "users#index"
   get "/users/:id" => "users#show"
-  push "/users" => "users#create"
+  post "/users" => "users#create"
   patch "/users/:id" => "users#update"
   delete "/users/:id" => "users#delete"
 end
